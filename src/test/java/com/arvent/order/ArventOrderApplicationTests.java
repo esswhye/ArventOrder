751f362f8e60190ad1094870e04b8c6ea9725fe9
esswhye <Chansengyong@mozat.com>
2020-01-28 18:26:59 +0800

    Arvent Order-Service

---
+++ src/test/java/com/arvent/order/ArventOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arvent.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArventOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
